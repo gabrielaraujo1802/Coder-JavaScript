4835011338b19c10bb78a66b6adf848ba264112b
Gabriel Araujo <gabrielcarvalho1802@gmail.com>
2023-07-04 19:58:45 -0300

    Desafio entregável - Aula 4

---
+++ aula2/trabalho/aula5.js
@@ -0,0 +1,9 @@
+const fruta = {
+  nome: "Maça",
+  peso: 0.2,
+  tipo: "Maça argentina",
+};
+
+console.log(fruta);
+
+console.log(fruta.nome);
---
+++ aula2/trabalho/trabaula4.js
@@ -0,0 +1,13 @@
+// Solicitando dois números ao usuário
+var numero1 = prompt("Digite o primeiro número:");
+var numero2 = prompt("Digite o segundo número:");
+
+// Convertendo os números para tipo float
+numero1 = parseFloat(numero1);
+numero2 = parseFloat(numero2);
+
+// Realizando a soma dos números
+var resultado = numero1 + numero2;
+
+// Exibindo o resultado da soma
+console.log("O resultado da soma é: " + resultado);
